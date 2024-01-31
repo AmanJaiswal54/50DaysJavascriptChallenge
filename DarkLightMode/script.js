dba14e1fb7cc799c1d0474675b88d7724545262a
Jaiswal <ajaiswal@comscore.com>
2024-02-01 02:05:51 +0530

    created Dark Light Mode

---
+++ DarkLightMode/script.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector('.btn');
+const container = document.querySelector('.container');
+btn.addEventListener('click', (e) =>{
+    container.classList.toggle('active');
+    e.target.innerText = e.target.innerText === 'Dark' ? 'Light': 'Dark';
+})
