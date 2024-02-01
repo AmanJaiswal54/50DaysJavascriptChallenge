1349e4340524a29522fa2908a538d9efa5275d3c
Jaiswal <ajaiswal@comscore.com>
2024-02-01 15:53:12 +0530

    added toggle for dark light mode

--- DarkLightMode/script.js
+++ DarkLightMode/script.js
@@ -1,6 +1,12 @@
-const btn = document.querySelector('.btn');
-const container = document.querySelector('.container');
-btn.addEventListener('click', (e) =>{
-    container.classList.toggle('active');
-    e.target.innerText = e.target.innerText === 'Dark' ? 'Light': 'Dark';
-})
+const btn = document.querySelector(".btn");
+const container = document.querySelector(".container");
+
+btn.addEventListener("click", (e) => {
+  container.classList.toggle("active");
+  e.target.innerText = e.target.innerText === "Dark" ? "Light" : "Dark";
+});
+
+document.querySelector(".wrapper").addEventListener("click", function (e) {
+  this.classList.toggle("active");
+  container.classList.toggle("active");
+});
