008d17df1a2e122bc7ce002cd57745faac6c9a1d
Jaiswal <ajaiswal@comscore.com>
2024-02-08 23:14:58 +0530

    How to add our own obj in prototype chaining

--- Prototypes/script.js
+++ Prototypes/script.js
@@ -33,3 +33,50 @@ Object.prototype.findTrimmedResultObj = function () {
 } 
 
 // ------------------------------------------------------------------------------------------
+
+//  How to add/insert our own object in between prototype chain
+//  before:  obj1 -> Object.prototype -> null
+
+
+let obj1 = {
+    name: 'vishnu'
+}
+
+let obj2 = {
+    name: 'aman'
+}
+
+obj2.__proto__ = obj1; //   1. using __proto__ (not good for JS engine code optimization)
+
+let obj3 = {
+    name: 'shiva'
+}
+
+Object.setPrototypeOf(obj3, obj2); //   2. using setPrototypeOf (modern method)
+
+let obj4 = Object.create(obj3, { // 3. using Object.create (modern method)
+    name: {
+        value: 'eshwar'
+    }
+})
+
+let obj5 = {
+    name: 'middle'
+}
+
+//  now:   obj4 -> obj3 -> obj2 -> obj1 -> Object.prototype -> null
+
+// ------------------------------------------------------------------------------------------
+
+//  insert obj5 in between
+//  before:   obj4 -> obj3 -> obj2 -> obj1 -> Object.prototype -> null
+
+Object.setPrototypeOf(obj3, obj5);
+Object.setPrototypeOf(obj5, obj2);
+
+
+//  now:   obj4 -> obj3 -> obj5 -> obj2 -> obj1 -> Object.prototype -> null
+
+
+// ------------------------------------------------------------------------------------------
+
