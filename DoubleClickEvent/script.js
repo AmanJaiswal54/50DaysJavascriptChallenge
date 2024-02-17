5be2341cd291bb412fca01c60468d170685bda56
Jaiswal <ajaiswal@comscore.com>
2024-02-17 19:30:04 +0530

    show animation on double click

--- DoubleClickEvent/script.js
+++ DoubleClickEvent/script.js
@@ -1,19 +1,37 @@
+let container = document.querySelector(".container");
 let initialTime = 0;
-document.querySelector(".grandParent").addEventListener("click", function (e) {
-    if(initialTime===0) {
-        initialTime = new Date().getTime();
+
+container.addEventListener("click", function (e) {
+  if (initialTime === 0) {
+    initialTime = new Date().getTime();
+  } else {
+    let newTime = new Date().getTime();
+    if (newTime - initialTime < 400) {
+      console.log("DOUBLE Clicked Created");
+      showAnimation(e);
+      initialTime = 0;
     } else {
-        let newTime = new Date().getTime();
-        if((newTime - initialTime) < 400) {
-            console.log('DOUBLE Clicked Created');
-            initialTime = 0;
-        } else {
-            initialTime = new Date().getTime();
-        }
+      initialTime = new Date().getTime();
     }
+  }
 });
 
+function showAnimation(e) {
+  let x = e.clientX - e.target.offsetLeft;
+  let y = e.clientY - e.target.offsetTop;
+  let circle = document.createElement("span");
+  circle.style.left = `${x}px`;
+  circle.style.top = `${y}px`;
+  setTimeout(() => {
+    circle.style.opacity = 0;
+  }, 20);
+  container.appendChild(circle);
 
-document.querySelector(".grandParent").addEventListener("dblclick", function (e) {
-    console.log('DOUBLE Clicked Inbuilt');
+  setTimeout(() => {
+    circle.remove();
+  }, 5000);
+}
+
+container.addEventListener("dblclick", function (e) {
+  console.log("DOUBLE Clicked Inbuilt");
 });
