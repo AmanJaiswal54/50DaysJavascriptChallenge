5f9f4e7dcf4166c16dc682bb30d126e39d99de2c
Jaiswal <ajaiswal@comscore.com>
2024-04-11 23:33:54 +0530

    added anagram

--- DummyPractice/mockInterview2.js
+++ DummyPractice/mockInterview2.js
@@ -169,3 +169,49 @@ function palindrome(str) {
 palindrome(str1);
 palindrome(str2);
 palindrome(str3);
+
+
+// -----------------------------
+
+// Anagram
+
+function anagram(str1, str2) {
+
+  if (str1.length !== str2.length) {
+    return "Not an Anagram";
+  }
+
+  let obj1 = {};
+  let obj2 = {};
+  for(let i=0;i<str1.length;i++) {
+    if(!obj1[str1[i]]) {
+      obj1[str1[i]] = 1; 
+    } else {
+      obj1[str1[i]] = obj1[str1[i]] + 1;
+    }
+  }
+
+  for(let i=0;i<str2.length;i++) {
+    if(!obj2[str2[i]]) {
+      obj2[str2[i]] = 1; 
+    } else {
+      obj2[str2[i]] = obj2[str2[i]] + 1;
+    }
+  }
+
+  for(let i=0;i<str1.length;i++) {
+    if(obj2[str1[i]] !== obj1[str1[i]]) {
+      return "Not an Anagram";
+    }
+  }
+
+  for(let i=0;i<str2.length;i++) {
+    if(obj2[str2[i]] !== obj1[str2[i]]) {
+      return "Not an Anagram";
+    }
+  }
+
+  return 'Its an anagram'
+}
+
+console.log(anagram("cheater", "teacher"));
