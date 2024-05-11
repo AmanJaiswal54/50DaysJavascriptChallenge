e9bde5f464b1c95adf5234a9c8b360a1c7891a37
Jaiswal <ajaiswal@comscore.com>
2024-05-12 01:12:31 +0530

    noteapp

--- NoteApp/script.js
+++ NoteApp/script.js
@@ -1,42 +1,69 @@
+let store = [];
+let uniqueId = 0;
 const notesContainer = document.querySelector(".notes-container");
 const btn = document.querySelector(".add-btn");
+
 btn.addEventListener("click", () => {
-  createNotes();
+  createNotes(uniqueId);
+  uniqueId++;
 });
 
-function createNotes() {
+function createNotes(uniqueId) {
   const div = document.createElement("div");
   div.classList.add("note");
+  div.classList.add(`note-${uniqueId}`);
   notesContainer.appendChild(div);
-  createBtnContainer(div);
-  createTextArea(div);
+  createBtnContainer(div, uniqueId);
+  createTextArea(div, uniqueId);
 }
 
-function createBtnContainer(noteParent) {
+function createBtnContainer(noteParent, uniqueId) {
   const div = document.createElement("div");
   div.classList.add("btn-container");
+  div.classList.add(`btn-container-${uniqueId}`);
   noteParent.appendChild(div);
-  createSavebtn(div);
-  createDeletebtn(div);
+  createSavebtn(div, uniqueId);
+  createDeletebtn(div, uniqueId);
 }
 
-function createSavebtn(parent) {
+function createSavebtn(parent, uniqueId) {
   const div = document.createElement("div");
   div.classList.add("save-btn");
+  div.classList.add(`save-btn-${uniqueId}`);
   div.innerHTML = "save";
+  div.addEventListener("click", function (e) {
+    let textAreaValue = document.querySelector(
+      `.textarea-container-${uniqueId}`
+    )?.value;
+    store[uniqueId] = textAreaValue;
+    console.log("store", store);
+    localStorage.setItem("note", JSON.stringify(store));
+  });
   parent.appendChild(div);
 }
 
-function createDeletebtn(parent) {
+function createDeletebtn(parent, uniqueId) {
   const div = document.createElement("div");
   div.classList.add("delete-btn");
+  div.classList.add(`delete-btn-${uniqueId}`);
   div.innerHTML = "delete";
+  div.addEventListener("click", function (e) {
+    let textAreaValues = JSON.parse(localStorage.getItem("note"));
+    let newtextAreaValues = textAreaValues.filter((value, i) => {
+      if (i !== uniqueId) {
+        return value;
+      }
+    });
+    document.querySelector(`.note-${uniqueId}`).remove();
+    // localStorage.removeItem(uniqueId);
+    localStorage.setItem("note", JSON.stringify(newtextAreaValues));
+  });
   parent.appendChild(div);
 }
 
-
-function createTextArea(noteParent) {
-    const textarea = document.createElement("textarea");
-    textarea.classList.add("textarea-container");
-    noteParent.appendChild(textarea);
+function createTextArea(noteParent, uniqueId) {
+  const textarea = document.createElement("textarea");
+  textarea.classList.add("textarea-container");
+  textarea.classList.add(`textarea-container-${uniqueId}`);
+  noteParent.appendChild(textarea);
 }
